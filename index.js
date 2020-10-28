231e9147949e66a345fa357fd35e2efe7f374ad0
Kai Carver <kaicarver@gmail.com>
2020-10-28 23:05:48 +0100

    switch to winter time

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ express()
   .get('/gettext', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query("SELECT TO_CHAR(created AT TIME ZONE 'CEST', 'YYYY/MM/DD') AS date, TO_CHAR(created AT TIME ZONE 'CEST', 'hh24:mi:ss') AS time, message FROM note ORDER BY created DESC");
+      const result = await client.query("SELECT TO_CHAR(created AT TIME ZONE 'CET', 'YYYY/MM/DD') AS date, TO_CHAR(created AT TIME ZONE 'CET', 'hh24:mi:ss') AS time, message FROM note ORDER BY created DESC");
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/gettext', results );
       client.release();
